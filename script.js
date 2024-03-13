817a97f339dd0bbbf9aeb4dc84e434d442c02b9a
Shraddha Jadav <shraddha.j@shaligraminfotech.com>
2024-03-13 13:51:44 +0530

    changes in script

--- script.js
+++ script.js
@@ -1,47 +1,6 @@
 var isNavOpen = true;
 
 function toggleNav() {
-  // if (isNavOpen) {
-  //   closeNav();
-  // } else {
-  //   openNav();
-  // }
-  // isNavOpen = !isNavOpen;
-
   $("#mySidenav").toggleClass("content--isHidden");
   $("#main").toggleClass("content--isHidden");
 }
-
-function openNav() {
-    var screenWidth = $(window).width();
-    if (screenWidth >= 576 && screenWidth <= 768) {
-      $("#mySidenav").css("width", "180px");
-      $("#main").css("margin-left", "180px");
-    } else {
-      $("#mySidenav").css("width", "320px");
-      $("#main").css("margin-left", "320px");
-    }
-    // $("#mySidenav a").css("display", "block");
-  }
-
-function closeNav() {
-  // $("#mySidenav a").css("display", "none");
-  $("#mySidenav").css("width", "0");
-  $("#main").css("margin-left", "0");
-}
-
-$(document).ready(function() {
-    $('.navbar-collapse').on('shown.bs.collapse', function () {
-        $('#main').css({
-            'margin-top': '360px',
-            'transition': 'margin-top 0.5s ease'
-        });
-    });
-
-    $('.navbar-collapse').on('hidden.bs.collapse', function () {
-        $('#main').css({
-            'margin-top': '80px',
-            'transition': 'margin-top 0.5s ease'
-        });
-    });
-});
